faded31953e07b990b8dd59f91772bef10473b35
Bartłomiej Ciernia <cierniabartek0802@gmail.com>
2025-12-18 23:02:03 +0100

    changed contet height

--- rpg-combat-manager/app/layout.tsx
+++ rpg-combat-manager/app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-          {children}
+          <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
+            {children}
+          </div>
         <Footer />
       </body>
     </html>
